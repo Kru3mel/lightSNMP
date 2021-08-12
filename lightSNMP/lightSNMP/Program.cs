e93f551cfe36423470a7043f43bb9fde4f5cc74f
mjay <malte.jung@online.de>
2021-08-12 13:14:41 +0200

    added UPD Listener
    -added asynchronous method to receive UDP packets
    -added UDP to SNMP Trap conversion

--- lightSNMP/lightSNMP/Program.cs
+++ lightSNMP/lightSNMP/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
+using System.Net;
+using System.Net.Sockets;
+using SnmpSharpNet;
 
 namespace lightSNMP
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
+            UDPListener();
+
+            while (true)
+            {
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(10000);
+            }
+            
+        }
+
+        static void UDPListener()
+        {            
+            Task.Run(() =>
+            {
+                using(UdpClient client = new UdpClient(162))
+                {
+                    Console.WriteLine("Listening");
+                    while (true)
+                    {
+                        IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 162);
+                        byte[] receivedBytes = client.Receive(ref endPoint);
+
+                        Console.WriteLine(endPoint.Address);
+                        SnmpV2Packet packet = new SnmpV2Packet();
+                        packet.decode(receivedBytes, receivedBytes.Length);
+                        Console.WriteLine(packet.ToString());
+                    }
+                }
+            });
         }
     }
 }
