198cb1b31332b663efd9339b8fe73ffe68b846b0
mjay <malte.jung@online.de>
2021-08-16 10:56:17 +0200

    Update AxisCamera.cs
    -added comments
    -added variables for OIDs

--- lightSNMP/lightSNMP/classes/AxisCamera.cs
+++ lightSNMP/lightSNMP/classes/AxisCamera.cs
@@ -10,27 +10,47 @@ namespace lightSNMP
 {
     class AxisCamera
     {
+        string axisOID = "1.3.6.1.4.1.368";
+        string axisAlarmOID = "1.3.6.1.4.1.368.4.2.0.3";
+
         public AxisCamera()
         {
 
         }
 
+        /// <summary>
+        /// Unique ID to specify alarm type
+        /// </summary>
         public int AlarmID { get; private set; } 
 
+        /// <summary>
+        /// Name of Alarm of SNMP-Trap
+        /// </summary>
         public string AlarmName { get; private set; }
 
+        /// <summary>
+        /// Text send with alarm SNMP- Trap
+        /// </summary>
         public string AlarmText { get; private set; }
 
+        /// <summary>
+        /// Camara ID extracted from Alarm Text
+        /// </summary>
         public int KameraID { get; private set; }
 
+        /// <summary>
+        /// Takes in a SNMP Trap and tries to extract the class parameters
+        /// </summary>
+        /// <param name="packet"></param>
+        /// <returns></returns>
         public bool parseTrap(SnmpV2Packet packet)
         {
-            if (!packet.Pdu.TrapObjectID.ToString().Contains("1.3.6.1.4.1.368"))
+            if (!packet.Pdu.TrapObjectID.ToString().Contains(axisOID))
             {
                 Console.WriteLine("Packet does not originate from axis device");
                 return false;                
             }
-            else if (!(packet.Pdu.TrapObjectID.ToString() == "1.3.6.1.4.1.368.4.2.0.3"))
+            else if (!(packet.Pdu.TrapObjectID.ToString() == axisAlarmOID))
             {
                 Console.WriteLine("Packet does not qualify as alarm");
                 return false;
