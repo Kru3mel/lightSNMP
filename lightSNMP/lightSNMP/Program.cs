95d96e793600837176b1adc35cddd041a7cd4f15
mjay <malte.jung@online.de>
2021-08-16 10:58:27 +0200

    Update Program.cs
    -added support to read values from app.config
    -added try- catch to packet decode process
    -added comments

--- lightSNMP/lightSNMP/Program.cs
+++ lightSNMP/lightSNMP/Program.cs
@@ -9,16 +9,27 @@ using System.Net;
 using System.Net.Sockets;
 using SnmpSharpNet;
 
+using System.Configuration;
+using System.Collections.Specialized;
+
 namespace lightSNMP
 {
     public class Program
     {               
         static void Main(string[] args)
         {
-            MBSecure mbSecure = new MBSecure("10.2.10.99", "read", "write");
+            //Get the needed values from config file
+            string ip = ConfigurationManager.AppSettings.Get("mbsecureIP");
+            string readCommunity = ConfigurationManager.AppSettings.Get("mbsecureReadCommunity");
+            string writeCommunity = ConfigurationManager.AppSettings.Get("mbsecureWriteCommunity");
+
+            //initialise mbsecure object
+            MBSecure mbSecure = new MBSecure(ip, readCommunity, writeCommunity);
             mbSecure.ResetInput();
+
+            //Start UDP-Listener in seperat Thread
             StartUDPListener(mbSecure);
-            //mBSecure.SetInput(1, 0);
+
             Console.Read();
         }
 
@@ -26,6 +37,7 @@ namespace lightSNMP
         {            
             Task.Run(() =>
             {
+                //Start UPD (SNMP) Listerner on port 162 in new Tread
                 using(UdpClient client = new UdpClient(162))
                 {
                     Console.WriteLine("Listening");
@@ -34,23 +46,37 @@ namespace lightSNMP
                         IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 162);
                         byte[] receivedBytes = client.Receive(ref endPoint);
 
+                        //Write IP Address from packet sender to consol
                         Console.WriteLine(endPoint.Address);
-                        SnmpV2Packet packet = new SnmpV2Packet();
-                        packet.decode(receivedBytes, receivedBytes.Length);
-                        Console.WriteLine(packet.ToString());
 
-                        if (packet.IsNotification && packet.Pdu.Type == PduType.V2Trap)
+                        try
                         {
-                            Task.Run(() => TrapHandler(packet,mbSecure));
+                            //Decode received packet
+                            SnmpV2Packet packet = new SnmpV2Packet();
+                            packet.decode(receivedBytes, receivedBytes.Length);
+                            //Write decodes packet to consol
+                            Console.WriteLine(packet.ToString());
+
+                            //start Trap Handler in seperat thread if received packet is a SNMPV2 Trap
+                            if (packet.IsNotification && packet.Pdu.Type == PduType.V2Trap)
+                            {
+                                Task.Run(() => TrapHandler(packet, mbSecure));
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Could not decode packet");
                         }
                     }
                 }
             });
         }
-
+        
         static void TrapHandler(SnmpV2Packet packet, MBSecure mbSecure)
         {
+            //initialise camera objcet
             AxisCamera camera = new AxisCamera();
+            //set corresponding input in MBSecure if trap signals alarm
             if (camera.parseTrap(packet)){
                 mbSecure.SetInput(camera.KameraID, 1);
             }
